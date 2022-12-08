a2b7dc19604a8a5494df47a684a9992143f2b402
scrazzz <scruzism@mail.com>
2022-12-08 09:38:31 +0300

    fizzbuzz

---
+++ fizzbuzz/main.nim
@@ -0,0 +1,9 @@
+for i in 0..100:
+    if i mod 15 == 0:
+        echo "fizzin' nd buzzin'"
+    elif i mod 3 == 0:
+        echo "fizzin'"
+    elif i mod 5 == 0:
+        echo "buzzin'"
+    else:
+        echo i
